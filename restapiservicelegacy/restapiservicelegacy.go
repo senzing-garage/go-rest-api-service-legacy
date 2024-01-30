0923666d10016ab0a62735cae27eb2de637c50a5
docktermj <michael@dockter.com>
2024-01-30 16:01:47 -0500

    #4 Savepoint

--- restapiservicelegacy/restapiservicelegacy.go
+++ restapiservicelegacy/restapiservicelegacy.go
@@ -5,6 +5,8 @@ import (
 	_ "embed"
 	"fmt"
 	"net/http"
+	"os"
+	"os/exec"
 )
 
 // ----------------------------------------------------------------------------
@@ -32,6 +34,15 @@ type RestApiServiceLegacyImpl struct {
 
 func (restApiServiceLegacyImpl *RestApiServiceLegacyImpl) Handler(ctx context.Context) *http.ServeMux {
 	rootMux := http.NewServeMux()
+
+	// Start Java
+
+	os.Setenv("PATH", "/home/sdk/jdk-11.0.16/bin:/home/temp/jtreg/bin:$PATH")
+	cmd, err := exec.Command("java", "-version").CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(cmd))
 
 	fmt.Print(">>>>>>>>>>>>>>>>>>>>>> HERE")
 
