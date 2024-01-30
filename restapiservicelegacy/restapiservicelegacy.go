7ff22976e32a5180a8bc37c5ff442f11dcac8fef
docktermj <michael@dockter.com>
2024-01-30 17:44:23 -0500

    #4 Savepoint

--- restapiservicelegacy/restapiservicelegacy.go
+++ restapiservicelegacy/restapiservicelegacy.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	_ "embed"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"os/exec"
@@ -26,9 +27,62 @@ type RestApiServiceLegacyImpl struct {
 // internal methods
 // ----------------------------------------------------------------------------
 
-// --- Logging ----------------------------------------------------------------
+var customTransport = http.DefaultTransport
 
-// --- Errors -----------------------------------------------------------------
+func init() {
+	// Here, you can customize the transport, e.g., set timeouts or enable/disable keep-alive
+}
+
+// --- xxxxxx -----------------------------------------------------------------
+
+// --- xxxxxx -----------------------------------------------------------------
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Printf(">>>>>> r = %+v\n", r)
+
+	// Create a new HTTP request with the same method, URL, and body as the original request
+
+	fmt.Printf(">>>>>> r.URL = %+v\n", r.URL)
+
+	proxyUrl := fmt.Sprintf("http://localhost:8260/%s", r.URL.RawPath)
+
+	fmt.Printf(">>>>>> proxyURL = %s\n", proxyUrl)
+
+	proxyReq, err := http.NewRequest(r.Method, proxyUrl, r.Body)
+	if err != nil {
+		http.Error(w, "Error creating proxy request", http.StatusInternalServerError)
+		return
+	}
+
+	// Copy the headers from the original request to the proxy request
+	for name, values := range r.Header {
+		for _, value := range values {
+			proxyReq.Header.Add(name, value)
+		}
+	}
+
+	// Send the proxy request using the custom transport
+	resp, err := customTransport.RoundTrip(proxyReq)
+	if err != nil {
+		http.Error(w, "Error sending proxy request", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Copy the headers from the proxy response to the original response
+	for name, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(name, value)
+		}
+	}
+
+	// Set the status code of the original response to the status code of the proxy response
+	w.WriteHeader(resp.StatusCode)
+
+	// Copy the body of the proxy response to the original response
+	io.Copy(w, resp.Body)
+}
 
 // --- Services ---------------------------------------------------------------
 
@@ -44,7 +98,8 @@ func (restApiServiceLegacyImpl *RestApiServiceLegacyImpl) Handler(ctx context.Co
 	}
 	fmt.Println(string(cmd))
 
-	fmt.Print(">>>>>>>>>>>>>>>>>>>>>> HERE")
+	// Proxy HTTP requests.
 
+	rootMux.HandleFunc("/", handleRequest)
 	return rootMux
 }
